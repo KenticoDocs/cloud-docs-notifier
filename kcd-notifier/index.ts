dd68de97c44c11db724eac34476491895b146fa1
Miroslav Kubus <MiroslavK@kentico.com>
2019-06-13 13:37:10 +0200

    KA-547 Setup tslint

--- kcd-notifier/index.ts
+++ kcd-notifier/index.ts
@@ -1,19 +1,18 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = async (context: Context, req: HttpRequest): Promise<void> => {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
 
     if (name) {
         context.res = {
+            body: 'Hello ' + (req.query.name || req.body.name),
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
         };
-    }
-    else {
+    } else {
         context.res = {
+            body: 'Please pass a name on the query string or in the request body',
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
         };
     }
 };
