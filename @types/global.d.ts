88717d0d1a06783c424d0421d2b8a3857b0c5015
Miroslav Kubus <MiroslavK@kentico.com>
2019-06-24 10:29:04 +0200

    KA-547 Change function type to eventGridTrigger

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -3,3 +3,14 @@ interface IHttpResponse {
     readonly body?: any;
     readonly [key: string]: any;
 }
+
+export interface IEventGridEvent {
+    readonly id: string;
+    readonly topic?: string;
+    readonly subject: string;
+    readonly data: any;
+    readonly eventType: string;
+    readonly eventTime: Date;
+    readonly metadataVersion?: string;
+    readonly dataVersion: string;
+}
